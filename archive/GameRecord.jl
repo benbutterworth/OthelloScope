1e935c9415b902f781e76c3423675a18ed51e478
benbutterworth <127899580+benbutterworth@users.noreply.github.com>
2024-07-28 11:16:01 +0100

    export into archive
    removing from src because i don't like how i started. want to retain the information for reference but not delete

---
+++ archive/GameRecord.jl
@@ -0,0 +1,5 @@
+abstract type Record end
+
+struct GameRecord
+    moves::Tuple{OthelloCoord}
+end
---
+++ archive/OthelloScope.jl
@@ -0,0 +1,7 @@
+module OthelloScope
+
+include("OthelloCoord.jl")
+include("OthelloBoard.jl")
+include("GameRecord.jl")
+
+end # module OthelloScope
