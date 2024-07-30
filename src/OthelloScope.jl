7b28f12a3f306212a67835d2fe32a109bca008ae
benbutterworth <127899580+benbutterworth@users.noreply.github.com>
2024-07-30 22:14:19 +0100

    Update OthelloScope.jl
    alter pseudocode comments

--- src/OthelloScope.jl
+++ src/OthelloScope.jl
@@ -1,5 +1,9 @@
 module OthelloScope
 
+include("isValidBoard.jl")
+include("boardtostring.jl")
+include("Board.jl")
+
 # example record: Jacob vs Ben 4 July 2024 d3c5e6f3d6c6e3f5f4f7f6g5c4e2g3c2c3d2g4h3f2b5b4b3c1h4e7f1a4b6h6h5h2d1g6a3a5a6c7c8e8d7e1g8d8b1b8g7h7h8a7h1a2f8b2a8b7a1g2g1
 
 #=
@@ -10,13 +14,13 @@ module OthelloScope
     - regex replace a-h w. numbers 1-8
     - split "a2b5d9" into pieces [(a,2), (b,5), (d,9)]
     - assert each coordinate references a place on the board
-            for i in 1:2:length(gamerecord)
-                first = Int(gamerecord[i]) - 96
-                second = parse(Int64, gamerecord[i+1])
-                push!(coordinates, (first, second))
-            end
-3 - PUT PIECES ON THE BOARD IN ORDER
-4 - RETURN THE FINAL GRID
+3 - SET UP THE GAME BOARD
+    - Place pieces in center 4 spots
+4 - PUT PIECES ON THE BOARD IN ORDER
+    - check is a valid move i.e. surrounded by at least one piece
+    - check is flanking other pieces.
+    - 
+5 - RETURN THE FINAL GRID
 
 =#
 end
