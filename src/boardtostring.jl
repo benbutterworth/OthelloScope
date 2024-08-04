84c5d8ca821368831dced9be3202d202ee055b96
benbutterworth <127899580+benbutterworth@users.noreply.github.com>
2024-08-04 13:39:37 +0100

    Update boardtostring.jl
    rewritten for clarity and conciseness. now generates string and does not print. modular.

--- src/boardtostring.jl
+++ src/boardtostring.jl
@@ -3,113 +3,108 @@ const col = " | "
 const rcol = " |"
 const newln = "\n"
 
+
 """
-    print_table_top(colwidths::Vector{Int8})
-Print the top line of a table with collumn widths `colwidths`.
+    row_divider(colwidths, position::String)
+
 """
-function print_table_top(io::IO, colwidths::Vector{Int8})
-    nlines = length(colwidths) - 1
-    print(io::IO, '┌')
-    for i in 1:nlines
-        print(io::IO, '─'^(colwidths[i] + 2))
-        print(io::IO, '┬')
+function row_divider(colwidths, position::String)
+    @assert colwidths isa Union{Tuple, Vector}
+    @assert eltype(colwidths) <: Integer
+
+    # Determine appropriate characters for row
+    if position == "top"
+        leftedgechar = '┌'
+        divchar = '┬'
+        rightedgechar = '┐'
+    elseif position == "middle"
+        leftedgechar = '├'
+        divchar = '┼'
+        rightedgechar = '┤'
+    elseif position == "bottom"
+        leftedgechar = '└'
+        divchar = '┴'
+        rightedgechar = '┘'
+    else
+        @error "`position` not valid, must be either `top`, `middle` or `bottom`."
     end
-    print(io::IO, '─'^(colwidths[end]+2))
-    print(io::IO, '┐', newln)
+
+    output::String = ""
+    nlines = length(colwidths) - 1    
+
+    output *= leftedgechar
+
+    for i in 1:nlines
+        colwidth = colwidths[i] + 2
+        output *= ('─'^colwidth) * divchar
+    end
+
+    endcolwidth = colwidths[end]+2
+    output *= ('─'^endcolwidth) * rightedgechar 
+    output *= newln
+end
+
+
+"""
+    row_contents(colwidths, textarray)
+
+"""
+function row_contents(colwidths, textarray)
+    @assert colwidths isa Union{Tuple, Vector}
+    @assert eltype(colwidths) <: Integer
+    @assert textarray isa Vector{String}
+    @assert length(colwidths) == length(textarray)
+
+    nlines = length(colwidths) - 1
+    output::String = ""
+    output *= lcol
+
+    for i in 1:nlines
+        output *= rpad(textarray[i], colwidths[i]) * col
+    end
+
+    output *= rpad(textarray[end], colwidths[end])
+    output *= rcol * newln
 end
 
 """
-    print_table_divider(colwidths::Vector{Int8})
-Print a divider line of a table with collumn widths `colwidths`.
+    get_collumn_widths(tablecontents)
 """
-function print_table_divider(io::IO, colwidths::Vector{Int8})
-    # tuple contains integers that repr. length of each column
-    nlines = length(colwidths) - 1
-    print(io::IO, '├')
-    for i in 1:nlines
-        print(io::IO, '─'^(colwidths[i] + 2))
-        print(io::IO, '┼')
-    end
-    print(io::IO, '─'^(colwidths[end]+2))
-    print(io::IO, '┤', newln)
-end
+function get_collumn_widths(tablecontents)
+    @assert tablecontents isa Matrix #can't print 3d tables
 
-"""
-    print_row_contents(colwidths::Vector{Int8}, textarray)
-Print the contents of an array `textarray` using `colwidths` to detemine spacing.
-"""
-function print_row_contents(io::IO, colwidths::Vector{Int8}, textarray)
+    nrows, ncols = size(tablecontents)
+    colwidths::Vector{Int64} = zeros(ncols)
 
-    if length(colwidths) != length(textarray)
-        @error "length of table does not match length of textarray."
-    end
-
-    nlines = length(colwidths) - 1
-
-    print(io::IO, lcol)
-    for i in 1:nlines
-        print(io::IO, rpad(textarray[i], colwidths[i]), col)
-    end
-    print(io::IO, rpad(textarray[end], colwidths[end]))
-    print(io::IO, rcol, newln)
-end
-
-"""
-    print_table_bottom(colwidths::Vector{Int8})
-Print the bottom line of a table with collumn widths `colwidths`.
-"""
-function print_table_bottom(io::IO, colwidths::Vector{Int8})
-    nlines = length(colwidths) - 1
-    print(io::IO, '└')
-    for i in 1:nlines
-        print(io::IO, '─'^(colwidths[i] + 2))
-        print(io::IO, '┴')
-    end
-    print(io::IO, '─'^(colwidths[end]+2))
-    print(io::IO, '┘', newln)
-end
-
-"""
-    get_collumn_widths(array) -> Vector{Int8}
-Return a list containing the width required to display the contents of an array. 
-"""
-function get_collumn_widths(array)
-    # @assert array isa Array{T,2} where {T<:Union{Char, String}}
+    stringtablecontents = map(string, tablecontents)    
 
     maxwidth(stringlist) = maximum(map(length, stringlist))
 
-    ncols = length(array[1]) # ? size(array)?
-    colwidths::Vector{Int8} = zeros(ncols)
-
-    for i in 1:ncols
-        # define accessor function for this
-        getcol(a) = a[i]
-        col = map(getcol, array) # to isolate column
-        col = map(string, map(getcol, array))
-        colwidths[i] = maxwidth(col) # find max wdith within a column
+    for col in 1:ncols
+        colwidth = maxwidth(stringtablecontents[:,col])
+        colwidths[col] = colwidth
     end
 
-    return colwidths
+    colwidths
 end
 
 """
-    print_ascii_table(mat::Matrix)
-Print the contents of a matrix in a pretty ascii table to `stdout`.
+    generate_table(tableinput::Matrix)
 """
-function print_ascii_table(io::IO, mat::Matrix)
-    nrows, _ = size(mat)
-    array = [Tuple(mat[i,:]) for i in 1:nrows]
+function generate_table(tableinput::Matrix)
+    stringtableinput = map(string, tableinput)
+    colwidths = get_collumn_widths(stringtableinput)
 
-    collumnwidths = get_collumn_widths(array)
-    nrows = length(array)
+    tableoutput::String = ""
+    tableoutput *= row_divider(colwidths, "top")
 
-    print_table_top(io, collumnwidths)
-
-    for i in 1:(nrows-1)
-        print_row_contents(io, collumnwidths, array[i])
-        print_table_divider(io, collumnwidths)
+    nrows, _ = size(tableinput)
+    for row in 1:(nrows - 1)
+        tableoutput *= row_contents(colwidths, stringtableinput[row,:])
+        tableoutput *= row_divider(colwidths, "middle")
     end
+    tableoutput *= row_contents(colwidths, stringtableinput[end, :])
+    tableoutput *= row_divider(colwidths, "bottom")
 
-    print_row_contents(io, collumnwidths, array[end])
-    print_table_bottom(io, collumnwidths)
+    tableoutput
 end
