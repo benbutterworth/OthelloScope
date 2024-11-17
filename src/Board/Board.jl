063cf98174372a1cb7900c2a3e11310948438421
benbutterworth <127899580+benbutterworth@users.noreply.github.com>
2024-11-17 17:14:44 +0000

    feat(Board): new type system for boards
    add BoundedBoard and TopologicalBoard. BoundedBoard is planar,
    TopologicalBoard is 3D surface w. connections for play to wrap around.

--- src/Board/Board.jl
+++ src/Board/Board.jl
@@ -3,10 +3,47 @@ export Board
 abstract type AbstractBoard end
 
 """
-    Board <: AbstractBoard
+Bounded Board <: AbstractBoard
+
+An Othello board with rigid play boundaries which moves do not wrap around.
+"""
+abstract type BoundedBoard <: AbstractBoard end
+
+"""
+SquareBoard <: BoundedBoard
+
+Square playing boards for Othello.
+"""
+abstract type SquareBoard <: BoundedBoard end
+
+
+"""
+IrregularBoard <: BoundedBoard
+
+Non-square playing boards for Othello that may contain holes.
+"""
+abstract type IrregularBoard <: BoundedBoard end
+
+"""
+LatticeBoard <: BoundedBoard
+
+Othello boards with non-cartestian indexing systems
+"""
+abstract type LatticeBoard <: BoundedBoard end
+
+"""
+TopologicalBoard <: AbstractBoard
+
+Othello boards without rigid boundaries that lets play happen on arbitrarily connected 3D Surfaces (e.g. surface of a cube, surface of a cylinder, torous.) 
+"""
+abstract type TopologicalBoard <: AbstractBoard end
+
+
+"""
+    Board <: BoundedBoard
 Struct representing the state of an Othello board.
 """
-struct Board <: AbstractBoard
+struct Board <: BoundedBoard
     state::Matrix{Int8} # 1 = Black, 0 = empty, -1 = White
     pieces::Tuple{Char,Char,Char} #character repr. of Black, White, Empty.
 end
