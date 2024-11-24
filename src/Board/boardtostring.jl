bebb683b9961e7aee1f2b844cac3ea660fae6ec7
benbutterworth <127899580+benbutterworth@users.noreply.github.com>
2024-11-24 23:21:14 +0000

    fix(boardtostr): add IO and char mapping to print_board

--- src/Board/boardtostring.jl
+++ src/Board/boardtostring.jl
@@ -78,16 +78,25 @@
 # end
 # 
 """
-    print_board(array::AbstractArray)
+    print_board(io::IO, board::Matrix)
 """
-function print_board(array::AbstractArray)
-    @assert length(size(array)) == 2
-    cols, rows = size(array)
-    println(" ", join(collect(1:cols)))
-    for row in 1:rows
-        println(row)
-        for col in 1:cols
-            print(array[row,col])
+function print_board(io::IO, board::Matrix)
+    newstate = map(board) do piece
+        if piece == 0
+            "-"
+        elseif piece ==1
+            "O"
+        elseif piece == -1
+            "X"
         end
+    end 
+    cols, rows = size(newstate)
+    println(io, " ", join(collect(1:cols)))
+    for row in 1:rows
+        print(io, row)
+        for col in 1:cols
+            print(io, newstate[row,col])
+        end
+        println(io)
     end 
 end
