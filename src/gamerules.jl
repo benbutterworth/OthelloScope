c8d4047860627b4be7a2d6a6b340c72cc5078a92
benbutterworth <127899580+benbutterworth@users.noreply.github.com>
2024-11-30 23:57:51 +0000

    feat(gamerules): skeleton funcs

---
+++ src/gamerules.jl
@@ -0,0 +1,15 @@
+function flip_pieces(board::AbstractBoard, position::Tuple{Int, Int}, colour::Symbol)
+    # 1. Check that the position is unoccupied, position is in bounds
+    # 2. Check occupying this position is a legal move
+    # 3. Identify which pieces are flipped by this move
+    # 4. Return altered board state after the move
+    return nothing
+end
+
+function find_legal_moves(board::AbstractBoard, position::Tuple{Int, Int}, colour::Symbol)
+   return nothing
+end
+
+function identify_flipped_pieces(board::AbstractBoard, position::Tuple{Int, Int}, colour::Symbol)
+   return nothing 
+end
