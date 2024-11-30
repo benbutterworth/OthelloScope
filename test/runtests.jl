5b97f8d36e82f69133eaad87a4f69349bc33a1fa
benbutterworth <127899580+benbutterworth@users.noreply.github.com>
2024-11-30 11:14:34 +0000

    rm(runtests): remove redundant test sets for Board

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,20 +3,6 @@ using OthelloScope
 
 const examplerecord = "d3c5e6f3d6c6e3f5f4f7f6g5c4e2g3c2c3d2g4h3f2b5b4b3c1h4e7f1a4b6h6h5h2d1g6a3a5a6c7c8e8d7e1g8d8b1b8g7h7h8a7h1a2f8b2a8b7a1g2g1"
 
-@testset "isValidBoard" begin
-    obj1 = ""
-    obj2 = fill(' ', 8, 8)
-    obj3 = fill(0, 8, 8)
-    obj4::Matrix{Int8} = fill(3, 8, 9)
-    obj4::Matrix{Int8} = rand(-1:1, 8, 8)
-
-    @test !isValidBoard(obj1) # Not Matrix
-    @test !isValidBoard(obj2) # Not parametric Matrix{Int8}
-    @test !isValidBoard(obj3) # Not parametric Matrix{Int8}
-    @test !isValidBoard(obj4) # Not (8, 8)
-    @test isValidBoard(obj5)  # ✓
-end
-
 @testset "record2coords" begin
     obj1 = 0
     obj2 = "d3 c5 e6"
