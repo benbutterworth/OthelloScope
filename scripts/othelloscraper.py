089e389eaefdbd7265269ceba0a30740113bf377
Ben Butterworth <sw19145@bristol.ac.uk>
2025-02-14 10:24:43 +0000

    add name formating and url list to othelloscraper

--- scripts/othelloscraper.py
+++ scripts/othelloscraper.py
@@ -1,6 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+# get all pages to script
+url_root = "https://www.worldothello.org/about/tournaments/world-othello-championship/woc/"
+urls = [url_root + str(i) + "/crosstable/13" for i in range(1988, 2026)]
+
+# players names shortened to
+def format_player_name(playerName):
+    moniker = playerName.split().reverse()
+    moniker[0] = moniker[0].upper()
+    moniker = ' '.join(moniker)
+    return moniker[:13]
+
 # Get URL input
 url = str(input("URL to Scrape: "))
 try:
